e39ba6a2c84fa6efc8f53b884977e22d701646f6
Laurin <laurin.zubler@ost.ch>
2022-11-13 22:48:29 +0100

    action bar

--- app/src/main/java/ch/ost/rj/mge/audio_cutter/activities/AudioActivity.java
+++ app/src/main/java/ch/ost/rj/mge/audio_cutter/activities/AudioActivity.java
@@ -1,12 +1,14 @@
 package ch.ost.rj.mge.audio_cutter.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
-import java.util.Objects;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
 import ch.ost.rj.mge.audio_cutter.R;
 import ch.ost.rj.mge.audio_cutter.model.Audio;
@@ -14,6 +16,8 @@ import ch.ost.rj.mge.audio_cutter.model.Audio;
 public class AudioActivity extends AppCompatActivity {
 
     private static final String AUDIO_KEY = "audio";
+
+    private Audio audio;
 
     public static Intent createIntent(Context context, Audio audio) {
         Intent intent = new Intent(context, AudioActivity.class);
@@ -26,7 +30,30 @@ public class AudioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio);
 
-        Audio audio = (Audio) getIntent().getSerializableExtra(AUDIO_KEY);
-        Objects.requireNonNull(getSupportActionBar()).setTitle(audio.name);
+        audio = (Audio) getIntent().getSerializableExtra(AUDIO_KEY);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(audio.name);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_delete:
+                return true;
+
+            case R.id.action_edit:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
